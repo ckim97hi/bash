6396498abb87e4ce21c8e837b2161a535f7ff377
danielrk <danielrheekim@gmail.com>
2014-12-01 10:34:04 -0500

    cd

--- process.c
+++ process.c
@@ -31,67 +31,108 @@ void process (CMD *cmdList)
     // SET VARIABLE********************
     if (pcmd->type == SIMPLE) {
         
-
-        if ((pid = fork()) < 0)
-            errorExit("SIMPLE",EXIT_FAILURE);
-
-        else if (pid == 0) {     // child
+        // cd
+        if (strcmp(*(pcmd->argv),"cd") == 0) {
             
-            // RED_IN
-            if (pcmd->fromType == RED_IN) {
-                int in = open(pcmd->fromFile, O_RDONLY);
-                if (in == -1) 
-                    error_Exit(pcmd->fromFile,errno);
-
-                dup2(in, 0);
-                close(in);
-            }
-            
-            // RED_OUT, RED_APP
-            if (pcmd->toType != NONE) {
-                int obits = O_CREAT | O_WRONLY;
-                if (pcmd->toType == RED_APP)
-                    obits = obits | O_APPEND;
-
-                int out = open(pcmd->toFile, obits, 0644);
-
-                if (out == -1)
-                    error_Exit(pcmd->toFile,errno);
-
-                dup2(out, 1);
-                close(out);
-
-            }
-            
-            // Set local variables only in this child process
-            if (pcmd->nLocal > 0) {
-                for (int i = 0; i < pcmd->nLocal; i++) 
-                    setenv(*((pcmd->locVar)+i), *((pcmd->locVal)+i), 1);
+            if (pcmd->argc > 2) {
+                fprintf(stderr, "usage: cd  OR  cd <directory-name>\n");
+                setenv("?","1",1);
+                return;
             }
 
+            else {
 
-            // Run command
-            execvp(*(pcmd->argv), pcmd->argv);
-            error_Exit(*(pcmd->argv),errno);
+                // set DIR to directory
+                char *dir; 
+                if (pcmd->argc == 1) {
+                    dir = getenv("HOME");
+                    if (dir == NULL) {
+                        fprintf(stderr, "cd: $HOME variable not set\n");
+                        setenv("?","1",1);
+                        return;
+                    }
+                }
+                else if (pcmd->argc == 2)
+                    dir = *((pcmd->argv)+1);
+
+                if (chdir(dir) == -1) {
+                    perror("cd: chdir failed");
+                    char str[20];
+                    sprintf(str, "%d", errno);
+                    setenv("?",str,1);
+                }
+                return;
+            }
         }
-        else {                   // parent
-            waitpid(pid, &status, 0); // **************CHANGE TO WAITPID IF MORE THAN 1 CHILD POSSIBLE
+        
+        // wait
+//        else if (strcmp(*(pcmd->argv),"
 
-            // Set $? to status
-            int program_status = (WIFEXITED(status) ? WEXITSTATUS(status) : 128+WTERMSIG(status));
-            char str[20];
-            sprintf(str, "%d", program_status);
-            setenv("?",str,1);
-            //close(0);
+        // Other commands (dirs, external)
+        else {
+            if ((pid = fork()) < 0)
+                errorExit("SIMPLE",errno);
+
+            else if (pid == 0) {     // child
+                
+                // RED_IN
+                if (pcmd->fromType == RED_IN) {
+                    int in = open(pcmd->fromFile, O_RDONLY);
+                    if (in == -1) 
+                        error_Exit(pcmd->fromFile,errno);
+
+                    dup2(in, 0);
+                    close(in);
+                }
+                
+                // RED_OUT, RED_APP
+                if (pcmd->toType != NONE) {
+                    int obits = O_CREAT | O_WRONLY;
+                    if (pcmd->toType == RED_APP)
+                        obits = obits | O_APPEND;
+
+                    int out = open(pcmd->toFile, obits, 0644);
+
+                    if (out == -1)
+                        error_Exit(pcmd->toFile,errno);
+
+                    dup2(out, 1);
+                    close(out);
+
+                }
+                
+                // Set local variables only in this child process
+                if (pcmd->nLocal > 0) {
+                    for (int i = 0; i < pcmd->nLocal; i++) 
+                        setenv(*((pcmd->locVar)+i), *((pcmd->locVal)+i), 1);
+                }
+
+
+                // Built-in command? (dirs)
+
+                // Run external command
+                execvp(*(pcmd->argv), pcmd->argv);
+                error_Exit(*(pcmd->argv),errno);
+            }
+            else {                   // parent
+                waitpid(pid, &status, 0); // **************CHANGE TO WAITPID IF MORE THAN 1 CHILD POSSIBLE
+
+                // Set $? to status
+                int program_status = (WIFEXITED(status) ? WEXITSTATUS(status) : 128+WTERMSIG(status));
+                char str[20];
+                sprintf(str, "%d", program_status);
+                setenv("?",str,1);
+                //close(0);
+            }
         }
     }
-
+    
     // PIPE
     // need to do status
     else if (pcmd->type == PIPE) {
         
         if (pipe(fd) == -1)
-            errorExit("pipe",EXIT_FAILURE);
+            errorExit("pipe",EXIT_FAILURE); // CHANGE TO STATUS
 
         if ((pid = fork()) < 0)
             errorExit("PIPE",EXIT_FAILURE);
@@ -125,6 +166,9 @@ void process (CMD *cmdList)
 
     else if (pcmd->type == SUBCMD) {
     }
+
+    // IF YOU PUT ANYTHING OUTSIDE THESE CONDITIONALS
+    // MAKE SURE RETURN STATEMENTS ARE EVERYWHERE NECESSARY
         
 
 }
